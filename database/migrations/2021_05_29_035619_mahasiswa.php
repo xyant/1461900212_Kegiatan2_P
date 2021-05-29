e70303d53449f18ecc1ac009def33a05dca65347
xyantm <ramaramadhan12@gmail.com>
2021-05-29 11:29:39 +0700

    first commit

---
+++ database/migrations/2021_05_29_035619_mahasiswa.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Mahasiswa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa',function(Blueprint $table){
+            $table->increments('id');
+            $table->string('nbi',10);
+            $table->string('nama_mhs',30);
+            $table->timestamps();
+
+        });
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
